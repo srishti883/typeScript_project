e829e251c513011e19e99e8346de31356e532b94
srishti <srishti.singh36sri@gmail.com>
2023-12-05 14:37:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// @ts-ignore
+function multi(n1, n2) {
+    return n1 * n2;
+}
+console.log(multi(10, 2));
+// @ts-ignore
+var language = 'JavaScript';
+language = 'PHP';
+console.log(language);
+// @ts-ignore
+var isTrue = false;
+console.log(isTrue);
---
+++ app.ts
@@ -0,0 +1,18 @@
+
+// @ts-ignore
+function multi(n1:number,n2:number){
+    return n1*n2
+}
+console.log(multi(10,2));
+
+
+// @ts-ignore
+let language = 'JavaScript';
+
+
+language = 'PHP';
+
+console.log(language); 
+// @ts-ignore
+let isTrue:boolean=false
+console.log(isTrue);
